13257a79e67945e011c5c94322ac01cd2165cbe9
Jendrik <jendrik@madewithtea.com>
2020-09-18 15:26:42 +0200

    options cycling

--- src/Options.js
+++ src/Options.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {FontIcon, ThemeIcon, FontSizeIcon} from './Icons.js';
-
 import {zooms, fonts, themes} from './theme.js'
+
+const Option = ({what, init, options, icon, onChange}) => {
+  const [state, setState] = useState(init)
+ 
+  const len = options.length
+  const handle = () => {
+    setState(state => {
+      const ns = state == options.length - 1? 0 : state + 1
+      const nv = {[what]: options[Object.keys(options)[ns]]}
+      onChange(s => ({...s, ...nv}))
+
+      console.log(ns)
+      return(ns)
+    })
+  }
+
+  return(<span onClick={() => handle()}>{icon}</span>)
+}
 
 export default ({setTheme}) => {
   const registry = 
-    [{what:'font', options: fonts},
-     {what:'theme', options: themes},
-     {what:'zoom', options: zooms}]
+    [{what:'font', options: fonts, icon: <FontIcon/>},
+     {what:'theme', options: themes, icon: <ThemeIcon/>},
+     {what:'zoom', options: zooms, icon: <FontSizeIcon/>}]
   return(
     <div id="options">
-    <FontIcon/>
-    <ThemeIcon/>
-    <FontSizeIcon/>
-
      {registry.map((k,i) => 
-      <select name={k.what} key={k.what} onChange={e => {
-        const nv = {[k.what]: k.options[e.target.value]}
-        setTheme(s => ({...s, ...nv}))
-      }}>
-      {Object.keys(k.options).map(o=> {
-        return(<option key={o} value={o}>{o}</option>)
-      })}
-      </select>
+      <Option what={k.what} 
+       init={0} 
+       key={i}
+       options={k.options} 
+       icon={k.icon} 
+       onChange={setTheme}/>
     )}
     </div>
   )
