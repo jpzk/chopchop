e39d7ffc4e4fefe2243520ab7a4ec624ae451fa9
Jendrik <jendrik@madewithtea.com>
2020-09-26 18:47:26 +0200

    added solarized dark/light

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,14 @@
 export const themes = {
+  solarizedDark: {
+    foreground: '#eee8d5',
+    fadedForeground: '#586e75',
+    background: '#073642',
+    selectedFg: '#000',
+    selectedBg: '#268bd2',
+    selectedLineBg: '#002b36',
+    optionFg: '#268bd2'
+  },
+
   apollo: {
     foreground: '#66606b',
     fadedForeground: '#eee',
@@ -16,6 +26,15 @@ export const themes = {
     selectedBg: '#ccc',
     selectedLineBg: '#ddd',
     optionFg: '#000'
+  },
+  solarizedLight: {
+    foreground: '#002b36',
+    fadedForeground: '#93a1a1',
+    background: '#fdf6e3',
+    selectedFg: '#fdf6e3',
+    selectedBg: '#6c71c4',
+    selectedLineBg: '#eee8d5',
+    optionFg: '#cb4b16'
   },
   dark: {
     foreground: '#fff',
